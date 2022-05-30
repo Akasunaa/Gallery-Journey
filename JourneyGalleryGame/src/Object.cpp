943e45124db26e3d0c16f1976592070afb1c99be
alexandrelaferrere <alexlafererre@gmail.com>
2022-05-30 19:50:30 +0200

    global object class implemented

---
+++ JourneyGalleryGame/src/Object.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Alexandre Laferrère on 30/05/2022.
+//
---
+++ JourneyGalleryGame/src/Object.h
@@ -0,0 +1,8 @@
+//
+// Created by Alexandre Laferrère on 30/05/2022.
+//
+
+#ifndef GALLERYJOURNEY_OBJECT_H
+#define GALLERYJOURNEY_OBJECT_H
+
+#endif //GALLERYJOURNEY_OBJECT_H
