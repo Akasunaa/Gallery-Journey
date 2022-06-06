65074417005d089137e3db43931ec68e15ae36dc
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-06-06 15:09:30 +0200

    potibug

--- JourneyGalleryGame/src/Door.cpp
+++ JourneyGalleryGame/src/Door.cpp
@@ -1,11 +1,11 @@
 #include "Door.h"
 
-Door::Door(b2World* world, float x, float y, GameAssets* ga):
-	InterractableObject{ world, x,y, widthDoor,heightDoor, ga }
+Door::Door(b2World* world, float x, float y, GameAssets* ga, Player* player):
+InterractableObject {world, x, y, widthDoor, heightDoor, ga}
 {
 	bodyDef.type = b2_dynamicBody;
 	rect.setPosition(x, y);
-	rect.setSize(sf::Vector2f(widthDoor,heightDoor));
+	rect.setSize(sf::Vector2f(widthDoor, heightDoor));
 	rect.setFillColor(sf::Color::Black);
 }
 
